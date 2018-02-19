4eb753799e6c21006d433a1ed20550a0a265ee08
Antonio <antoniomone97@gmail.com>
2018-02-19 12:04:35 +0100

    Initial Commit

---
+++ ScenarioGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScenarioGame
+//
+//  Created by Antonio Mone on 19/02/18.
+//  Copyright © 2018 Antonio Mone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
